c9fc67dacc305269ecb7afb56105a26b764566b5
newman-cheng <nc2893@columbia.edu>
2021-11-29 14:07:59 -0500

    Initial Commit

---
+++ authorlist.js
@@ -0,0 +1,54 @@
+const authorlist = [
+    {
+        name: "Junyi Huang",
+        institution: "Columbia University",
+        url: ""
+    },
+    {
+        name: "Maxwell Benjamin Strome",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/maxwell-strome-778401150/"
+    },
+    {
+        name: "Ian Jenkins",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/ipj/"
+    },
+    {
+        name: "Parker Williams",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/parkerawilliams/"
+    },
+    {
+        name: "Bo Feng",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/bo-feng-cu/"
+    },
+    {
+        name: "Yaning Wang",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/liamyaningwang/"
+    },
+    {
+        name: "Roman Wang",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/roman-wang-1019a77a/"
+    },
+    {
+        name: "Vaibhav Bagri",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/vaibhav-bagri/"
+    },
+    {
+        name: "Newman Cheng",
+        institution: "Columbia University",
+        url: "https://www.linkedin.com/in/newman-cheng/"
+    },
+    {
+        name: "Iddo Drori",
+        institution: "Massachusetts Institute of Technology",
+        url: "https://www.linkedin.com/in/iddodrori/"
+    }
+];
+
+export default authorlist;
---
+++ figures.js
@@ -0,0 +1,15 @@
+const figures = {
+    "F1": {
+        "image": "./figures/face-relationships.jpg",
+        "caption": "",
+        "width": "80%",
+        "maxWidth": "800px"
+    },
+    "F2": {
+        "image": "./figures/architecture.jpg",
+        "caption": "",
+        "width": "80%"
+    }
+};
+
+export default figures;
