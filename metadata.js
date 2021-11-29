50df76aa3b4caa3b6ff63f6526dcec896106fa91
newman-cheng <95239134+newman-cheng@users.noreply.github.com>
2021-11-29 15:35:29 -0500

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -1,5 +1,5 @@
 const metadata = {
-    img: "./cuseas.png",
+    img: "",
     abstract: "Kinship verification is the task of determining whether a parent-child, sibling, or grandparent-grandchild relationship exists between two people and is important in social media applications, forensic investigations, finding missing children, and reuniting families. We demonstrate high quality kinship verification by participating in the 2021 Recognizing Families in the Wild challenge which provides the largest publicly available dataset in the field. Our approach is among the top 3 winning entries in the competition. We ensemble models written by both human experts and a foundation model, OpenAI Codex, trained on text and code. We use Codex to generate model variants, and also demonstrate its ability to generate entire running programs for kinship verification tasks of specific relationships.",
     tldr: "First use of OpenAI Codex for winning top 3 in an international common task framework competition.",
     venue: "IEEE International Conference on Automatic Face and Gesture Recognition",
